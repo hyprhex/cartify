8eedba6f0e2aa551dc6bf9badfd7f12a8c1fac7a
hyprhex <swapapis@gmail.com>
2024-04-08 08:20:39 +0100

    Setup HTTP server

---
+++ cmd/api/api.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	log.Println("Start server at port", s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
